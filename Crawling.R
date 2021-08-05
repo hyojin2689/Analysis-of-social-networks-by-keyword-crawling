7e7d2c9b1d564784c372dd5a160b542e3cec6124
hyojin2689 <80669371+hyojin2689@users.noreply.github.com>
2021-08-05 23:29:16 +0900

    Create Crawling.R

---
+++ Crawling.R
@@ -0,0 +1,20 @@
+blog <- c()
+
+for (i in 1: 30) {
+  GET(url = "https://section.blog.naver.com/ajax/SearchList.nhn",
+      query = list("countPerPage" = "7",
+                   "currentPage" = i,
+                   "endDate" = "2020-12-31",
+                   "keyword" = "제로웨이스트",
+                   "orderBy" = "sim",
+                   "startDate" = "2020-01-01",
+                   "type" = "post"),
+      add_headers("referer" = "https://section.blog.naver.com/Search/Post.nh")) %>% 
+      httr::content(as = "text") %>% str_remove(pattern = '\\)\\]\\}\',') %>% fromJSON() -> naverBlog
+  
+  data <- naverBlog$result$searchList
+  blog <- bind_rows(blog, data) 
+  
+  cat(i, "번째 페이지 크롤링 완료\n")
+  Sys.sleep(time = 3)
+}
